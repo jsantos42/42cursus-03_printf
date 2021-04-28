9312d34e2af5253d61c53f30cbf70275a755d15f
Joao Santos <jsantos@c1r20s10.42lisboa.com>
2021-04-28 18:08:30 +0100

    comment out

--- precision.c
+++ precision.c
@@ -42,31 +42,6 @@ char	*check_precision(char *string, t_flags *flags)
 	else
 		return (precision_not_smaller(string, flags));
 }
-/*
-** incorrect explanation!!! change it
-**
-** if precision >len && precision < min_width, adds (precision - len)
-** number of zeros, and the rest is padding ' '
-** if precision > min width, padding = '0' * precision e leftlustify 0
-** e min width = precision
-**
-** if precision is specified and smaller than what we want to print,
-** we have two different behaviours depending on what we are printing:
-**   if it's a string, then it might cut it (therefore we change flags->len);
-**   if it's a int or hex, it doesn't cut it, it only decreases the
-** padding (therefore, we change flags->min_width);
-*/
-
-/* acrescentei + negative porque quando e negativo nos aumentamos o len para
-** incluir o sinal neativo e, se a diferenca entre precision e len for de
-** apenas 1, faz com que entre neste if erradamente
-**
-** acrescentei aqui para o caso de querer imprimir 0 com precisao 0 nao ser
-** suposto dar nada
-**
-** acrescentei aqui +1 se negativo porque acrescentamos 1 ao len na printf_d
-** acrescentei este if para retirar o caracter do menos
-*/
 
 char	*precision_smaller(char *string, t_flags *flags)
 {
